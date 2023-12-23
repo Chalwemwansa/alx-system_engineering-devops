52f00da422604b3dfaae3657ba9844929d43b0c6
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-23 04:47:15 +0200

    changed task1 file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,9 @@
 # module that helps install flask onto the machine
 # the version is 2.1.0 from pip3
 
-class { 'flask':
-  version => '2.1.0',
-}
-
-package { 'python3-flask':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
-  require  => Class['flask'],
+  name     => 'python3-flask',
+  provider => 'pip3',
 }
 
