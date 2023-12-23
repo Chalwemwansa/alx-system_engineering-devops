abd561674661036aa56e037a37df26dfebebab0a
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-23 04:39:46 +0200

    checking task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 # module that helps install flask onto the machine
 # the version is 2.1.0 from pip3
 
-package { 'flask':
+class { 'flask':
+  version => '2.1.0',
+}
+
+package { 'python3-flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+  require  => Class['flask'],
 }
+
