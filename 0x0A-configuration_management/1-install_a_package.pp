ab027c12f0cd3fe26f6224c394803c8fe7541f26
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-23 04:53:07 +0200

    updated the file1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,8 @@
 # module that helps install flask onto the machine
 # the version is 2.1.0 from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'python3-flask',
-  provider => 'pip3',
+exec { 'flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/bin']
 }
 
