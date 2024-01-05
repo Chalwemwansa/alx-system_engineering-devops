d6e81bf9ddf576b85f3a5ff6bd0fa4ae1cfd8d2c
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-05 04:07:12 +0200

    used file_line for extra task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,22 @@
 # puppet script that changes the configuraation of the /etc/ssh/ssh_config file
 
-#the ssh path
+#include the stdlib for file_line
+include stdlib
+
+#path to file
 $ssh_path = '/etc/ssh/ssh_config'
 
-# the commands variable to store the commands in an array
-$commands = ["sed -i.bak 's/^#* *PasswordAuthentication *.*/    PasswordAuthentication No/g' ${ssh_path}",
-  "sed -i 's/Host \\*/Host \\*\\n    IdentityFile ~\\/\\.ssh\\/school/g' ${ssh_path}"]
+#use file_line after installation of augeas tools
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => $ssh_path,
+  line   => '    PasswordAuthentication No',
+  match  => '^#* *PasswordAuthentication *.*',
+}
 
-#the exec resource to be used to execute the commands
-exec { 'ssh_configurations':
-  command => $commands.join('; '),
-  path    => ['/bin', '/usr/bin'],
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => $ssh_path,
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^[^#]*IdentityFile.*',
 }
