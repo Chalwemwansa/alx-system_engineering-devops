20d7fc861966dce776d085a40a4e601171e24008
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-05 02:58:21 +0200

    made changes to task7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,19 +2,29 @@
 # requirements;
 # listen on port 80, a redirection when redirect_me is visited
 
+# installation of the package nginx
+package { 'nginx':
+  ensure   => installed,
+  provider => apt,
+}
+
 # the path variable
 $nginx_path = '/etc/nginx/sites-available/default'
 
 # the commands in a commands variable
-$commands = ['apt -y update && apt -y reinstall nginx',
-  "sed -i.bak 's/listen [0-9]\\{1,\\}/listen 80/g' ${nginx_path}",
+$commands = ["sed -i.bak 's/listen [0-9]\\{1,\\}/listen 80/g' ${nginx_path}",
   "sed -i 's/listen \\[::\\]:[0-9]\\{1,\\}/listen \\[::\\]:80/g' ${nginx_path}",
   "sed -i 's/^[^#]*location \\/ {/\\tlocation = \\/ {\\n\\t\\treturn 200 \"Hello World!\\\\n\";/g' ${nginx_path}",
   "sed -i 's/^[^#]*server {/server {\\n\\tlocation \\/redirect_me {\\n\\t\\treturn 301 http:\\/\\/localhost;\\n\\t}/g' ${nginx_path}",
-  'service nginx start']
-
+            ]
+#executes the commands in the commands variable
 exec { 'nginx_configuration':
   command => $commands.join('; '),
   path    => ['/bin', '/usr/bin'],
   timeout => 600,
+}
+
+#starts the service
+service { 'nginx':
+  ensure => running,
 }
