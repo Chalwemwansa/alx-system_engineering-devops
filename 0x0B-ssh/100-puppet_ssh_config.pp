f6e5d345026bcb7c769bd86b25b52a35f968fa55
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-05 03:34:26 +0200

    made changes to the extra task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
 # puppet script that changes the configuraation of the /etc/ssh/ssh_config file
 
-augeas { 'ssh_client_configuration':
-  context => '/etc/ssh/ssh_config',
-  changes => [
-    "set identityFile ~/.ssh/school",
-    "set PasswordAuthentication no",
-  ],
+#the ssh path
+$ssh_path = '/etc/ssh/ssh_config'
+
+# the commands variable to store the commands in an array
+$commands = ["sed -i.bak 's/^#* *PasswordAuthentication *.*/    PasswordAuthentication No/g' ${ssh_path}",
+  "sed -i 's/Host \\*/Host \\*\\n    IdentityFile ~\\/\\.ssh\\/school/g' ${ssh_path}"]
+
+#the exec resource to be used to execute the commands
+exec { 'ssh_configurations':
+  command => $commands.join('; '),
+  path    => ['/bin', '/usr/bin'],
 }
