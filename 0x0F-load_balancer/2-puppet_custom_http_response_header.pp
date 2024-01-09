b3d4bb2d53856fcb7a86b6fe4194e6f49b0edc84
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-09 23:08:32 +0200

    checking puppet task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,13 +4,14 @@
 
 package {'nginx':
   ensure   => installed,
-  provider => 'apt',
+  provider => apt,
 }
 
 exec { 'nginx_config':
   command => "sed -i.bak \"s/^[^#]* *server *{/server {\\n\\tadd_header X-Served-By $(hostname);/g\" /etc/nginx/sites-available/default",
   path    => ['/bin', '/usr/bin'],
-  unless  => "grep -q 'add_header X-Served-By' /etc/nginx/sites-available/default",}
+  unless  => "grep -q 'add_header X-Served-By' /etc/nginx/sites-available/default",
+}
 
 service { 'nginx':
   ensure => 'running',
