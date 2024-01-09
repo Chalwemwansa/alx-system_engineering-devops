c1e5e6f2106dd718c256b62ba749030f352e695d
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-09 23:49:53 +0200

    added more to puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,10 +6,14 @@ package {'nginx':
   ensure   => installed,
   provider => apt,
 }
-$command = "sed -i.bak \"s/^[^#]* *server *{/server {\\n\\tadd_header X-Served-By $(hostname);/g\" /etc/nginx/sites-available/default"
+
+$command = ["sed -i.bak \"s/^[^#]* *server *{/server {\\n\\tadd_header X-Served-By $(hostname);/g\" /etc/nginx/sites-available/default",
+  "sed -i.bak 's/listen [0-9]\{1,\}/listen 80/g' /etc/nginx/sites-available/default",
+  "sed -i 's/listen \[::\]:[0-9]\{1,\}/ listen \[::\]:80/g' /etc/nginx/sites-available/default",
+  "sed -i 's/^[^\(#.*\)]location \/ {/\tlocation = \/ {\n\t\treturn 200 "Hello World!\\n";/g' /etc/nginx/sites-available/default"]
 
 exec { 'nginx_config':
-  command => $command,
+  command => $command.join('; '),
   path    => ['/bin', '/usr/bin'],
   unless  => "grep -q 'add_header X-Served-By' /etc/nginx/sites-available/default",
 }
