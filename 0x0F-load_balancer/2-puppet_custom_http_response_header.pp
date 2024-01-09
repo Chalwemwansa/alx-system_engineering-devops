bf7f1ceabec31a3b91ecd4591886bd53e3628039
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-09 23:55:49 +0200

    added more to puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,31 @@
-# this is a puppet script that configures a server according to some requirements
+# a puppet script that configures an ubuntu server
+# requirements;
+# listen on port 80, a redirection when redirect_me is visited
 
-# this is a puppet script that configures a server according to some requirements
-
-package {'nginx':
+# installation of the package nginx
+package { 'nginx':
   ensure   => installed,
   provider => apt,
 }
 
-$command = ["sed -i.bak \"s/^[^#]* *server *{/server {\\n\\tadd_header X-Served-By $(hostname);/g\" /etc/nginx/sites-available/default",
-  "sed -i.bak 's/listen [0-9]\{1,\}/listen 80/g' /etc/nginx/sites-available/default",
-  "sed -i 's/listen \[::\]:[0-9]\{1,\}/ listen \[::\]:80/g' /etc/nginx/sites-available/default",
-  "sed -i 's/^[^\(#.*\)]location \/ {/\tlocation = \/ {\n\t\treturn 200 "Hello World!\\n";/g' /etc/nginx/sites-available/default"]
+# the path variable
+$nginx_path = '/etc/nginx/sites-available/default'
 
-exec { 'nginx_config':
-  command => $command.join('; '),
+# the commands in a commands variable
+$commands = ["sed -i.bak 's/listen [0-9]\\{1,\\}/listen 80/g' ${nginx_path}",
+  "sed -i 's/listen \\[::\\]:[0-9]\\{1,\\}/listen \\[::\\]:80/g' ${nginx_path}",
+  "sed -i 's/^[^#]*location \\/ {/\\tlocation = \\/ {\\n\\t\\treturn 200 \"Hello World!\\\\n\";/g' ${nginx_path}",
+  "sed -i 's/^[^#]*server {/server {\\n\\tlocation \\/redirect_me {\\n\\t\\treturn 301 http:\\/\\/localhost;\\n\\t}/g' ${nginx_path}",
+  "sed -i.bak \"s/^[^#]* *server *{/server {\\n\\tadd_header X-Served-By $(hostname);/g\" ${nginx_path}"
+             ]
+#executes the commands in the commands variable
+exec { 'nginx_configuration':
+  command => $commands.join('; '),
   path    => ['/bin', '/usr/bin'],
-  unless  => "grep -q 'add_header X-Served-By' /etc/nginx/sites-available/default",
+  timeout => 600,
 }
 
+#starts the service
 service { 'nginx':
-  ensure => 'running',
+  ensure => running,
 }
